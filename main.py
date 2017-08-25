5edfc800be6cd9b7b83b921216871b4ff5217915
nathan <njgingrich@gmail.com>
2017-08-25 15:48:18 -0400

    properly parse json

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import requests
+import time
 
 def getHours(steam_id):
   url = 'http://localhost:8003/users/' + steam_id
@@ -7,6 +8,10 @@ def getHours(steam_id):
   response = requests.get(url)
   if response.ok:
     data = json.loads(response.content)
-    print(data)
+    return data['result']['playtimes']['totals']['two_weeks']
 
-getHours('76561198042101272')
+while True:
+  print('Getting hours for user')
+  hours = getHours('76561198042101272')
+  print('Found {0} hours'.format(hours))
+  time.sleep(30)
