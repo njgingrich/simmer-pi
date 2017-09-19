24ea428d32c785a49c93e4b35d5a1232e2f46762
nathan <njgingrich@gmail.com>
2017-09-19 02:22:02 -0400

    leds turn themselves off on exit

--- led.py
+++ led.py
@@ -1,7 +1,8 @@
 import RPi.GPIO as GPIO
 import time
+import atexit
 
-class LEDBar:
+class Bar:
   def __init__(self):
     self.minutes = 0
     self.leds = {1: 14, 2: 15, 3: 18, 4: 23, 5: 24, 6: 25, 7: 8, 8: 7}
@@ -10,36 +11,18 @@ class LEDBar:
     GPIO.setwarnings(False)
     for key, val in self.leds.items():
       GPIO.setup(val, GPIO.OUT)
-    #self.determine_leds()
+      atexit.register(self.turn_off_pin, val)
 
-  def determine_leds(self):
+  def determine_leds(self, minutes):
+    self.minutes = minutes
     # turn on LEDs 1 thru (minutes / 180)
     highest = int(self.minutes / 180)
-    for led in range(0, highest + 1):
-      self.turn_on_led(led)
+    for led in range(1, highest + 1):
+      print('turning on led {0}').format(led)
+      self.turn_on_pin(self.leds[led])
 
-  def turn_on_led(self, LED):
-    GPIO.output(self.leds[LED], GPIO.HIGH)
+  def turn_on_pin(self, pin):
+    GPIO.output(pin, GPIO.HIGH)
 
-  def turn_off_led(s6elf, LED):
-    GPIO.output(self.leds[LED], GPIO.LOW)
-
-
-def test():
-  GPIO.setmode(GPIO.BCM)
-  GPIO.setwarnings(False)
-  for key, val in leds.items():
-    print(key, val)
-    GPIO.setup(val, GPIO.OUT)
-
-  print('LED on')
-  for key, val in leds.items():
-    GPIO.output(val, GPIO.HIGH)
-    time.sleep(0.5)
-    GPIO.output(val, GPIO.LOW)
-  for key, val in leds.items():
-    GPIO.output(val, GPIO.HIGH)
-  time.sleep(2)
-  print('LED off')
-  for key, val in leds.items():
-    GPIO.output(val, GPIO.LOW)
+  def turn_off_pin(self, pin):
+    GPIO.output(pin, GPIO.LOW)
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import requests
 import time
 import led as LED
 
-def getHours(steam_id):
+def getMinutes(steam_id):
   url = 'http://localhost:8003/users/' + steam_id
 
   response = requests.get(url)
@@ -14,8 +14,8 @@ def getHours(steam_id):
 leds = LED.Bar()
 while True:
   print('Getting hours for user')
-  hours = getHours('76561198042101272')
-  print('Found {0} hours'.format(hours))
-  leds.determine_leds()
+  mins = getMinutes('76561198042101272')
+  print('Found {0} mins'.format(mins))
+  leds.determine_leds(mins)
   time.sleep(30)
 
